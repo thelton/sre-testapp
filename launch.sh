13f2273128ea29b953ab380c7cf127da73e90cae
TJ Helton <th3lton@gmail.com>
2023-05-22 16:14:20 -0700

    update launch to create env file. update compose with container for seeding postgres

--- launch.sh
+++ launch.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
- 
+export $DBHOST 
 CRED=$(aws secretsmanager get-secret-value --secret-id postgres-creds --region us-east-1 --output text --query SecretString)
 USER=$(echo $CRED | jq '.username' | tr -d '"')
-PASSWORD=$(echo $CRED | jq '.password' | tr -d '"')
+PASS=$(echo $CRED | jq '.password' | tr -d '"')
+
+echo $'HOST='$DBHOST \
+'\nDATABASE=students\n' \
+'USERNAME='$USER \
+'\nPASSWORD='$PASS > src/.env
 
 docker run ./dbseed.dockerfile -e USER=$USER -e HOST=$DBHOST
 
