dd6898e49d832facfcf1d69915230e5f7c8cddbc
TJ Helton <th3lton@gmail.com>
2023-05-22 21:33:53 -0700

    update seds

--- launch.sh
+++ launch.sh
@@ -3,10 +3,10 @@ CRED=$(aws secretsmanager get-secret-value --secret-id postgres-creds --region u
 export USER=$(echo $CRED | jq '.username' | tr -d '"')
 export PASS=$(echo $CRED | jq '.password' | tr -d '"')
 
-sed -i "0,/process.env.USERNAME/s//$USER/" /app/src/db.js
-sed -i "0,/process.env.HOST/s//$DBHOST/" /app/src/db.js
-sed -i "0,/process.env.DATABASE/s//students/" /app/src/db.js
-sed -i "0,/process.env.PASSWORD/s//$PASS/" /app/src/db.js
+sed -i "0,/process.env.USERNAME/s//'$USER'/" /app/src/db.js
+sed -i "0,/process.env.HOST/s//'$DBHOST'/" /app/src/db.js
+sed -i "0,/process.env.DATABASE/s//'students'/" /app/src/db.js
+sed -i "0,/process.env.PASSWORD/s//'$PASS'/" /app/src/db.js
 
 echo "Setting HOST=$DBHOST in /app/src/.env" 
 echo "HOST=$DBHOST" > /app/src/.env
