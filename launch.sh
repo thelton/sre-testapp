d5f2dc2b0390b5a2ad2cdde94ce19723f01bc7e6
TJ Helton <th3lton@gmail.com>
2023-05-22 19:11:28 -0700

    update scripts and clean nginx.conf

--- launch.sh
+++ launch.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
+export $DBHOST
 CRED=$(aws secretsmanager get-secret-value --secret-id postgres-creds --region us-east-1 --output text --query SecretString)
 export USER=$(echo $CRED | jq '.username' | tr -d '"')
 export PASS=$(echo $CRED | jq '.password' | tr -d '"')
-export DBHOST=sre-testapp-db.czmthj6yjt68.us-east-1.rds.amazonaws.com
 
 echo $"HOST=$DBHOST
 DATABASE=students
--- seed.sh
+++ seed.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-psql -h $HOST -d students -U $USER -c "CREATE TABLE IF NOT EXISTS students (id SERIAL PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), age INT, DOB DATE); ALTER TABLE students OWNER to $USER;"
+psql -h $HOST -d students -U $USER -c "CREATE TABLE IF NOT EXISTS students (id SERIAL PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), age INT, DOB DATE); ALTER TABLE students OWNER to $USER;INSERT INTO students(id, name, email, age, DOB) VALUES(1, 'John Smith', 'jsmith@jsmith.com', 24, '2023-05-18');"
