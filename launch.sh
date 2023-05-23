be1b9a1ed895d87e71bf01c462d517b84bb30e32
TJ Helton <th3lton@gmail.com>
2023-05-22 19:43:45 -0700

    update script to replace process.env not working

--- launch.sh
+++ launch.sh
@@ -4,9 +4,14 @@ CRED=$(aws secretsmanager get-secret-value --secret-id postgres-creds --region u
 export USER=$(echo $CRED | jq '.username' | tr -d '"')
 export PASS=$(echo $CRED | jq '.password' | tr -d '"')
 
+sed -i "0,/process.env.USERNAME/s//$USER/" /app/src/db.js
+sed -i "0,/process.env.HOST/s//$DBHOST/" /app/src/db.js
+sed -i "0,/process.env.DATABASE/s//students/" /app/src/db.js
+sed -i "0,/process.env.PASSWORD/s//$PASS/" /app/src/db.js
+
 echo $"HOST=$DBHOST
 DATABASE=students
 USERNAME=$USER
-PASSWORD=$PASS" > src/.env
+PASSWORD=$PASS" > /app/src/.env
 
 docker-compose up --build -d
