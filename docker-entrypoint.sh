7c80b552ad155ae86a5511a58265dcd003ba5c0b
TJ Helton <th3lton@gmail.com>
2023-05-22 17:49:05 -0700

    update scripts

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 psql -h $HOST -d students -U $USER -c "CREATE TABLE IF NOT EXISTS students (id SERIAL PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), age INT, DOB DATE); ALTER TABLE students OWNER to $USER;';"
+6Nxe8fCqLvLm8zSL
--- launch.sh
+++ launch.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-export $DBHOST 
+
 CRED=$(aws secretsmanager get-secret-value --secret-id postgres-creds --region us-east-1 --output text --query SecretString)
-USER=$(echo $CRED | jq '.username' | tr -d '"')
-PASS=$(echo $CRED | jq '.password' | tr -d '"')
+export USER=$(echo $CRED | jq '.username' | tr -d '"')
+export PASS=$(echo $CRED | jq '.password' | tr -d '"')
+export DBHOST=sre-testapp-db.czmthj6yjt68.us-east-1.rds.amazonaws.com
 
-echo $'HOST='$DBHOST \
-'\nDATABASE=students\n' \
-'USERNAME='$USER \
-'\nPASSWORD='$PASS > src/.env
-
-docker run ./dbseed.dockerfile -e USER=$USER -e HOST=$DBHOST
+echo $"HOST=$DBHOST
+DATABASE=students
+USERNAME=$USER
+PASSWORD=$PASS" > src/.env
 
 docker-compose up --build -d
